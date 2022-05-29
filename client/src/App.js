cd681f5445ffa3d6d822aa18c630610be0cbcc25
Phyf3 <phade075@gmail.com>
2022-05-29 12:14:24 +0100

    feat : done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
-const API_BASE = "http://localhost:3001"
+const API_BASE = "https://tewdhew.herokuapp.com"
 
 
 function App() {
